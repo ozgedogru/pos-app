03a5d383778891aa1de8a071d938125dd614f097
ozgedogru <ozgenurkarademir@gmail.com>
2024-10-26 15:09:34 +0200

    add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
       fontFamily: {
         sans: ["Nunito", "sans-serif"],
       },
+      colors: {
+        navy: "#0B192C",
+        beige: "#EEEEEE",
+        blue: "#1E3E62",
+        red: "#B8001F",
+      },
       gridTemplateColumns: {
         card: "repeat(auto-fill, minmax(150px, 2fr))",
       },
