04f2492393d0e1e2b5e6aa7bcdade9c77b732727
ozgedogru <ozgenurkarademir@gmail.com>
2024-10-26 15:08:51 +0200

    create cart section

---
+++ src/components/CartSummary.jsx
@@ -0,0 +1,71 @@
+import { Button } from "antd";
+import { ClearOutlined, MinusOutlined, PlusOutlined } from "@ant-design/icons";
+
+const CartSummary = () => {
+  return (
+    <div className="cart h-full max-h-[calc(100vh_-_96px)] flex flex-col">
+      <div className="bg-navy text-gray-100 p-4">
+        <h2 className="font-bold tracking-wide">Grocery Cart </h2>
+      </div>
+      <div className="cart-items px-4 py-2 overflow-y-auto">
+        <ul className="flex flex-col gap-y-2">
+          <li className="cart-item flex justify-between">
+            <div className="flex items-center">
+              <img
+                src="https://cdn.pixabay.com/photo/2023/01/25/18/46/broccoli-7744338_1280.jpg"
+                alt="product-img"
+                className="w-16 h-16 object-cover rounded"
+              ></img>
+              <div className="flex flex-col ml-2">
+                <b>Broccoli</b>
+                <span>6 Ft x 2</span>
+              </div>
+            </div>
+            <div className="flex items-center">
+              <Button
+                className="rounded-full"
+                icon={<MinusOutlined />}
+                size="small"
+              ></Button>
+              <span className="mx-2">2</span>
+              <Button
+                className="rounded-full"
+                icon={<PlusOutlined />}
+                size="small"
+              ></Button>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className="cart-totals mt-auto px-4 flex flex-col gap-4">
+        <div className="flex flex-col">
+          <div className="flex justify-between">
+            <span>Subtotal</span>
+            <span>99 HUF</span>
+          </div>
+          <div className="flex justify-between">
+            <span>VAT %8</span>
+            <span>+7.92 HUF</span>
+          </div>
+          <div className="flex justify-between text-xl border-t mt-2 pt-2">
+            <b>Total</b>
+            <span>106.92 HUF</span>
+          </div>
+        </div>
+        <div className="buttons flex flex-col justify-center gap-1">
+          <Button className="bg-blue text-white hover:text-blue font-semibold">
+            Complete
+          </Button>
+          <Button
+            className="bg-red text-white font-semibold"
+            icon={<ClearOutlined />}
+          >
+            Reset
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartSummary;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,13 +6,13 @@ import CartSummary from "../components/CartSummary";
 const HomePage = () => {
   return (
     <div className="px-8 flex justify-between gap-8">
-      <div className="categories flex-2 overflow-auto max-h-[calc(100vh-_120px)] pb-2">
+      <div className="categories overflow-auto max-h-[calc(100vh-_120px)] min-w-[140px] pb-2">
         <Categories />
       </div>
-      <div className="products flex-[8] border border-green-500">
+      <div className="products flex-[8]">
         <Products />
       </div>
-      <div className="cart-summary flex-1 border border-green-500">
+      <div className="cart-summary shadow-2xl flex-1 min-w-[300px] md:-mr-[24px] md:-mt-[24px]">
         <CartSummary />
       </div>
     </div>
