d5c90921c7dc057dcbfe25c0166d4f6c75a748fc
ozgedogru <ozgenurkarademir@gmail.com>
2024-11-06 09:26:41 +0100

    implement category get/post requests

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,23 +1,70 @@
-const Categories = () => {
-  const categories = [
-    "All Products",
-    "Fruits",
-    "Vegetables",
-    "Dairy Products",
-    "Meats",
-    "Desserts",
-    "Non-Alcoholic Drinks",
-    "Alcoholic Drinks",
-  ];
+import { Button, Form, Input, message, Modal } from "antd";
+import axios from "axios";
+import { useState } from "react";
+
+const Categories = ({ categories }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [form] = Form.useForm();
+
+  const onFinish = async (values) => {
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/api/categories/add-category",
+        values
+      );
+      message.success("A new category added!");
+      form.resetFields();
+      setIsModalOpen(false);
+    } catch (error) {
+      console.log(error);
+      message.error("Failed to add category. Please try again.");
+    }
+  };
 
   return (
     <div>
       <ul className="flex md:flex-col items-center md:font-semibold md:text-lg text-sm gap-2">
         {categories.map((category, index) => (
           <li key={index} className="category-item">
-            <span>{category}</span>
+            <span>{category.title}</span>
           </li>
         ))}
+        <li
+          className="category-item"
+          onClick={() => {
+            setIsModalOpen(true);
+          }}
+        >
+          + Add New Category
+        </li>
+        <Modal
+          title="Basic Modal"
+          open={isModalOpen}
+          onCancel={() => {
+            setIsModalOpen(false);
+          }}
+          footer={null}
+        >
+          <Form layout="vertical" onFinish={onFinish} form={form}>
+            <Form.Item
+              label="Add Category"
+              name="title"
+              rules={[
+                {
+                  required: true,
+                  message: "Category name cannot be empty.",
+                },
+              ]}
+            >
+              <Input placeholder="Enter category name" />
+            </Form.Item>
+            <Form.Item className="text-right">
+              <Button type="primary" htmlType="submit">
+                Create
+              </Button>
+            </Form.Item>
+          </Form>
+        </Modal>
       </ul>
     </div>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Categories from "../components/Categories";
 import Products from "../components/Products";
 import CartSummary from "../components/CartSummary";
 import Header from "../components/Header";
+import axios from "axios";
 
 const HomePage = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const getCategories = async () => {
+      try {
+        const res = await axios.get(
+          "http://localhost:5000/api/categories/get-all"
+        );
+        setCategories(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getCategories();
+  }, [categories]);
+
   return (
     <div className="min-h-screen">
       <Header />
       <div className="px-8 md:flex justify-between gap-8">
         <div className="categories overflow-auto max-h-[calc(100vh_-_7rem)] md:min-w-[140px] md:mx-1 pb-6">
-          <Categories />
+          <Categories categories={categories} />
         </div>
         <div className="products flex-[8] overflow-y-auto max-h-[calc(100vh_-_7rem)] pb-8">
           <Products />
