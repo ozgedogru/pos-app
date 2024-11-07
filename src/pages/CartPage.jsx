8b03d62bbf28299f9851203a6cc7b64dea838f8c
ozgedogru <ozgenurkarademir@gmail.com>
2024-11-07 15:09:17 +0100

    make cart page dynamic

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,75 +1,101 @@
-import { Button, Card, Table } from "antd";
+import { Button, Card, Popconfirm, Table } from "antd";
 import { useState } from "react";
 import CreateInvoice from "../components/CreateInvoice";
 import Header from "../components/Header";
+import { useDispatch, useSelector } from "react-redux";
+import { addItem, clearItem, removeItem } from "../features/cartSlice";
+import { MinusOutlined, PlusOutlined } from "@ant-design/icons";
 
 const CartPage = () => {
-  const dataSource = [
-    {
-      productName: "Apple",
-      category: "Fruits",
-      price: 12,
-      quantity: 3,
-      totalPrice: 36,
-      action: "Edit/Delete",
-    },
-    {
-      productName: "Banana",
-      category: "Fruits",
-      price: 8,
-      quantity: 5,
-      totalPrice: 40,
-      action: "Edit/Delete",
-    },
-    {
-      productName: "Milk",
-      category: "Dairy Products",
-      price: 15,
-      quantity: 2,
-      totalPrice: 30,
-      action: "Edit/Delete",
-    },
-    {
-      productName: "Carrot",
-      category: "Vegetables",
-      price: 5,
-      quantity: 6,
-      totalPrice: 30,
-      action: "Edit/Delete",
-    },
-  ];
+  const { items } = useSelector((state) => state.cart);
+
+  const dispatch = useDispatch();
+
   const columns = [
     {
+      title: "Product Image",
+      dataIndex: "img",
+      key: "img",
+      width: "4%",
+      render: (url) => {
+        return (
+          <img
+            src={url}
+            alt="product-image"
+            className="w-full h-16 object-cover"
+          />
+        );
+      },
+    },
+    {
       title: "Product Name",
-      dataIndex: "productName",
-      key: "productName",
+      dataIndex: "title",
+      key: "title",
+      width: "8%",
     },
     {
       title: "Category",
       dataIndex: "category",
       key: "category",
+      width: "8%",
     },
     {
       title: "Price",
       dataIndex: "price",
       key: "price",
+      width: "8%",
       render: (price) => `${price} HUF`,
     },
     {
       title: "Quantity",
       dataIndex: "quantity",
       key: "quantity",
+      width: "8%",
+      render: (_, record) => {
+        return (
+          <div className="flex items-center">
+            <Button
+              className="rounded-full"
+              icon={<MinusOutlined />}
+              size="small"
+              onClick={() => dispatch(removeItem(record))}
+              disabled={record.quantity === 1}
+            ></Button>
+            <span className="mx-2">{record.quantity}</span>
+            <Button
+              className="rounded-full"
+              icon={<PlusOutlined />}
+              size="small"
+              onClick={() => dispatch(addItem(record))}
+            ></Button>
+          </div>
+        );
+      },
     },
     {
       title: "Total Price",
       dataIndex: "totalPrice",
       key: "totalPrice",
+      width: "8%",
       render: (totalPrice) => `${totalPrice} HUF`,
     },
     {
       title: "Action",
-      dataIndex: "action",
-      key: "action",
+      width: "12%",
+      render: (_, record) => {
+        return (
+          <Popconfirm
+            title="Are you sure you want to delete this item?"
+            onConfirm={() => dispatch(clearItem(record))}
+            okText="Yes"
+            cancelText="No"
+          >
+            <Button type="link" className="text-red pl-0">
+              Delete
+            </Button>
+          </Popconfirm>
+        );
+      },
     },
   ];
 
@@ -79,7 +105,7 @@ const CartPage = () => {
     <div>
       <Header />
       <div className="px-6">
-        <Table dataSource={dataSource} columns={columns} bordered></Table>
+        <Table dataSource={items} columns={columns} bordered></Table>
         <div className="cart-total flex justify-end mt-8">
           <Card className="w-72 shadow-lg">
             <div className="flex justify-between">
